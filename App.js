91486e5613edd3b19bb1de072601e69d4d90e794
adtimokhin <adtimokhin@gmail.com>
2023-07-09 14:29:47 +0600

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-main flex-1 items-center justify-center">
+      <Text>Open up App.js todddp!</Text>
+      <Text>Open up App.js todddp!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
